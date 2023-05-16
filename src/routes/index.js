b6d162ec41432d9d56bcf5dac0f8203d73ee61ee
jeffry <jeffcb88@gmail.com>
2023-05-16 13:08:45 +0200

    Se limpia el código por claes innecesarias

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,9 @@ const api = express.Router();
 
 const userCtrl = require('../../src/controllers/user');
 const authCtrl = require('../../src/controllers/auth');
-
-const alertCtrl = require('../controllers/mail');
-const billFileList = require('../controllers/bill_files');
-const customerCtrl = require('../controllers/customer');
 const rentalHouseCtrl = require('../controllers/rental_house');
 const auth = require('../middlewares/auth');
-const logService = require('../middlewares/billable_services_log')
+//const logService = require('../middlewares/billable_services_log')
 
 
 api.get("/", (req, res) => {
@@ -24,24 +20,9 @@ api.get('/user/:id', userCtrl.findById);
 api.delete('/user/:id', auth, userCtrl.remove);
 
 
-
-
-
 //autenticaciones
 api.post('/singin', authCtrl.singIn);
 api.post('/singup', authCtrl.singUp);
-
-//archivos de facturas
-api.post('/bill_files', auth, billFileList.create);
-api.get('/bill_files', auth, billFileList.findAll);
-api.get('/bill_files/:bill_id', auth, billFileList.find);
-api.put('/bill_files/:bill_id', auth, billFileList.update);
-api.delete('/bill_files/:bill_id', auth, billFileList.remove);
-
-//archivos de facturas
-api.post('/customer', auth, customerCtrl.create);
-api.get('/customer', auth, customerCtrl.findAll);
-api.delete('/customer/:id', auth, customerCtrl.remove);
 
 //archivos de rental houses
 api.post('/rentalHouse', rentalHouseCtrl.create);
@@ -49,10 +30,6 @@ api.get('/rentalHouse', auth, rentalHouseCtrl.findAll);
 api.delete('/rentalHouse/:id', auth, rentalHouseCtrl.remove);
 
 
-api.post('/mail', auth, logService, alertCtrl.send);
-
-
-
-
+//api.post('/mail', auth, logService, alertCtrl.send);
 
 module.exports = api;
